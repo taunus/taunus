65f54118205991710ce8cc5d0dcc6874fac21d4d
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-05-13 18:56:42 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ponymoo
+
+> Micro MVC engine
+
+`ponymoo` aims to simplify the state of MVC and shared rendering.
+
+# License
+
+MIT
---
+++ ponymoo.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var contra = require('contra');
+var ponymoo = λ.emitter({
+  boot: require('./lib/boot')
+});
+
+module.exports = ponymoo;
