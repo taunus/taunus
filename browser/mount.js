c786ac5c29dfedb8a0c44ec955859fcc656ac966
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-05-14 21:48:07 -0300

    initial complete release

--- browser/mount.js
+++ browser/mount.js
@@ -6,6 +6,9 @@ var activator = require('./activator');
 
 function mount (container, routes) {
   var data = container.dataset.taunus;
+  if (!data) {
+    throw new Error('taunus: expected data-taunus attribute missing');
+  }
   var model = JSON.parse(data);
 
   state.container = container;
