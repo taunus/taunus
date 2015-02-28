7f900348f83f09590de7f039b216d869ca7aac2f
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-02-28 15:56:49 -0300

    updated xhr module

--- browser/xhr.js
+++ browser/xhr.js
@@ -2,34 +2,34 @@
 
 var xhr = require('xhr');
 
-function request (url, o, done) {
-  var options = {
+function request (url, options, done) {
+  var o = {
     url: url,
     json: true,
     headers: { Accept: 'application/json' }
   };
   if (done) {
-    Object.keys(o).forEach(overwrite);
+    Object.keys(options).forEach(overwrite);
   } else {
-    done = o;
+    done = options;
   }
 
-  global.DEBUG && global.DEBUG('[xhr] %s %s', options.method || 'GET', options.url);
+  global.DEBUG && global.DEBUG('[xhr] %s %s', o.method || 'GET', o.url);
 
-  var req = xhr(options, handle);
+  var req = xhr(o, handle);
 
   return req;
 
   function overwrite (prop) {
-    options[prop] = o[prop];
+    o[prop] = o[prop];
   }
 
   function handle (err, res, body) {
     if (err && !req.getAllResponseHeaders()) {
-      global.DEBUG && global.DEBUG('[xhr] %s %s aborted', options.method || 'GET', options.url);
+      global.DEBUG && global.DEBUG('[xhr] %s %s aborted', o.method || 'GET', o.url);
       done(new Error('aborted'), null, res);
     } else {
-      global.DEBUG && global.DEBUG('[xhr] %s %s done', options.method || 'GET', options.url);
+      global.DEBUG && global.DEBUG('[xhr] %s %s done', o.method || 'GET', o.url);
       done(err, body, res);
     }
   }
